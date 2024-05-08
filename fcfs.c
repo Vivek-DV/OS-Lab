967bcf95e8787bdb42946baee4b33805ab5c20be
Vivek D V <149104475+Vivek-DV@users.noreply.github.com>
2024-05-08 09:35:38 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,69 +1,52 @@
 #include<stdio.h>
-struct process{
-	int id,at,bt,tat,wt,ct;
-}arr[10];
 
-void read(int n)
-{
-	int i;
-	for(i=0;i<n;i++)
-	{
-		printf("Enter the id,arrival time and burst time of process %d\n",i+1);
-		scanf("%d%d%d",&arr[i].id,&arr[i].at,&arr[i].bt);
-	}
-}
+struct fcfs{
+    int id, at, bt, ct, wt, tat;
+} pro[100];
 
-void sort(int n)
-{
-	struct process p;
-	for(int i=0;i<n;i++)
-	{
-		for(int j=0;j<n-i-1;j++)
-		{
-			if(arr[j].at>arr[j+1].at)
-			{
-				p=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=p;
-			}
-		}
-	}
-}
-
-void FCFS(int n)
-{
-	int tbt=0;
-	float twt=0,ttat=0;
-	arr[0].wt=0;
-	arr[0].ct=arr[0].at+arr[0].bt;
-	arr[0].tat=arr[0].wt+arr[0].bt;
-	ttat+=arr[0].tat;
-	tbt+=arr[0].bt;
-	for(int i=1;i<n;i++)
-	{
-		arr[i].wt=tbt-arr[i].at;
-		arr[i].tat=arr[i].bt+arr[i].wt;
-		arr[i].ct=arr[i].bt+arr[i].wt+arr[i].at;
-		tbt+=arr[i].bt;
-		ttat+=arr[i].tat;
-		twt+=arr[i].wt;
-	}
-	float avg_wt=twt/n,avg_tat=ttat/n;
-	printf("Average waiting time: %f\nAverage turn around time: %f\n",avg_wt,avg_tat);
-	
-}
-
-int main()
-{
-	int n;
-	printf("Enter the number of process\n");
-	scanf("%d",&n);
-	read(n);
-	sort(n);
-	FCFS(n);
-	printf("ID\tAT\tBT\tCT\tWT\tTAT\n");
-	for(int i=0;i<n;i++)
-	{
-		printf("%d\t%d\t%d\t%d\t%d\t%d\n",arr[i].id,arr[i].at,arr[i].bt,arr[i].ct,arr[i].wt,arr[i].tat);
-	}
+int main() {
+    int n, i, j;
+    int tbt = 0;
+    float twt = 0, ttat = 0;
+    
+    printf("Enter the number of processes\n");
+    scanf("%d", &n);
+    
+    for(i = 0; i < n; i++) {
+        printf("Enter id, AT and BT of process %d\n", i + 1);
+        scanf("%d%d%d", &pro[i].id, &pro[i].at, &pro[i].bt);
+    }
+    
+    // Sorting processes based on arrival time using Bubble Sort
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n - i - 1; j++) {
+            if(pro[j].at > pro[j + 1].at) {
+                struct fcfs temp = pro[j];
+                pro[j] = pro[j + 1];
+                pro[j + 1] = temp;
+            }
+        }
+    }
+    
+    // Calculating completion time, waiting time, and turnaround time
+    for(i = 0; i < n; i++) {
+        pro[i].ct = tbt + pro[i].bt;
+        pro[i].tat = pro[i].ct - pro[i].at;
+        pro[i].wt = pro[i].tat - pro[i].bt;
+        tbt += pro[i].bt;
+        twt += pro[i].wt;
+        ttat += pro[i].tat;
+    }
+    
+    float avg_wt = twt / n;
+    float avg_tat = ttat / n;
+    
+    printf("Average wt: %f\nAverage tat: %f\n", avg_wt, avg_tat);
+    printf("ID\tAT\tBT\tCT\tWT\tTAT\n");
+    
+    for(i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", pro[i].id, pro[i].at, pro[i].bt, pro[i].ct, pro[i].wt, pro[i].tat);
+    }
+    
+    return 0;
 }
